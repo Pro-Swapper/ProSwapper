b8e1b2824edb34aaebe6d1dad805f1b037a115be
Kye <promasterboy@outlook.com>
2021-09-23 12:09:13 +1000

    a

--- src/UI/Dashboard.cs
+++ src/UI/Dashboard.cs
@@ -31,11 +31,11 @@ namespace Pro_Swapper
             newstext.Font = newstext.Font = new System.Drawing.Font("Segoe UI", float.Parse(api.apidata.newstext.Split(';')[1]), System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
 
             string AutoPatchNotes = string.Empty;
-            foreach (api.Item item in api.apidata.items.Skip(128))
+            foreach (api.Item item in api.apidata.items.Skip(142))
             {
                 AutoPatchNotes += $"Added {item.SwapsFrom} to {item.SwapsTo}\n";
             }
-            patchnotes.Text += api.apidata.patchnotes + Environment.NewLine + AutoPatchNotes;
+            patchnotes.Text += $"{api.apidata.patchnotes}{Environment.NewLine}{AutoPatchNotes}";
             try
             {
                 news.ImageLocation = msgpack.MsgPacklz4($"{api.FNAPIEndpoint}v2/news/br?responseFormat=msgpack_lz4&responseOptions=ignore_null").data.image;
@@ -44,7 +44,7 @@ namespace Pro_Swapper
         }
 
         private void yt_Click(object sender, EventArgs e)=> global.OpenUrl("https://youtube.com/proswapperofficial");
-         private void twitter_Click(object sender, EventArgs e)=> global.OpenUrl("https://twitter.com/Pro_Swapper");
+        private void twitter_Click(object sender, EventArgs e)=> global.OpenUrl("https://twitter.com/Pro_Swapper");
         private void discord_Click(object sender, EventArgs e)=> global.OpenUrl(api.apidata.discordurl);
         private void pictureBox1_Click(object sender, EventArgs e)=> global.OpenUrl("https://github.com/Pro-Swapper");
         private void pictureBox2_Click(object sender, EventArgs e)=> global.OpenUrl("https://tiktok.com/@proswapperofficial");
